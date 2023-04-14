687249951a39056e68ae9e464e514c8917d73dd0
ramirolugo25 <ramacap60@gmail.com>
2023-04-14 15:27:06 -0300

    add car Service

---
+++ src/module/car/service/carService.js
@@ -0,0 +1,56 @@
+const Car = require("../entity/car");
+const CardIdNotDefinedError = require("./error/carIdNotDefinedError");
+const CarNotDefinedError = require("./error/carNotDefinedError");
+
+module.exports = class Service {
+
+    /**
+     * 
+     * @param {import('../repository/abstractCarRepository')} carRepository 
+     */
+    constructor(carRepository){
+        this.carRepository = carRepository;
+    }
+
+    /**
+     * 
+     * @param {import('../entity/car')} car 
+     */
+
+    async save(car){
+        if(car === undefined){
+            throw new CarNotDefinedError();
+        }
+
+        return this.carRepository.save(car);
+    }
+
+
+    /**
+     * 
+     * @param {import('../entity/car')} car 
+     */
+    async delete(car){
+        if (!(car instanceof Car)){
+            throw new CarNotDefinedError();
+        }
+
+        return this.carRepository.delete(car);
+    }
+
+    /**
+     * 
+     * @param {Number} id 
+     */
+    async getById(id){
+        if (id === undefined){
+            throw new CardIdNotDefinedError();
+        }
+
+        return this.carRepository.getById(id);
+    }
+
+    async getAll(){
+        return this.carRepository.getAll();
+    }
+}
